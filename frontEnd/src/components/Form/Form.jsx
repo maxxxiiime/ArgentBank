33c6b843c5ce376fd2c2f122204f2ddfb25d3b5b
Maxime J <125677181+maxxxiiime@users.noreply.github.com>
2023-10-10 17:25:04 +0200

    Fetch login test 1

--- frontEnd/src/components/Form/Form.jsx
+++ frontEnd/src/components/Form/Form.jsx
@@ -1,21 +1,74 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./form.scss";
 
 
 export default function Form() {
+  const navigate = useNavigate(); 
+  // stock les valeurs du mail et du mdp
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // fonction pour gérer le submit du form
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Empêche le reload du form par défaut
+
+  // formData recup les donnés du form
+  const formData = {
+    email: email,
+    password: password,
+  };
+
+  try {
+    const response = await fetch("http://localhost:3001/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+        // Vérifie si la réponse HTTP est réussie (statut 200)
+        if (response.ok) {
+          // réponse en JSON
+          const responseData = await response.json();
+          console.log("Réponse de l'API :", responseData);
+          navigate("/user"); // Redirige vers la page user
+        } else {
+          console.error("Erreur lors de la requête API :", response.statusText);
+        }
+      } catch (error) {
+        console.error("Erreur lors de la requête API :", error);
+      }
+    };
+  
+
+
   return (
     <div>
         <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
 
-<form>
+<form onSubmit={handleSubmit}>
           <div className="input-wrapper">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input type="text" 
+            id="username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+             />
           </div>
           <div className="input-wrapper">
+
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input type="password" 
+            id="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
