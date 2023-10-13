5c4861dfcc14d0a1777a9d1fe3303fb069a3464d
Maxime J <125677181+maxxxiiime@users.noreply.github.com>
2023-10-13 09:04:37 +0200

    test fetch profile fail

--- frontEnd/src/page/User/User.jsx
+++ frontEnd/src/page/User/User.jsx
@@ -1,13 +1,66 @@
+import React, { useEffect, useState } from "react";
 import "./user.scss";
 
 export default function User() {
+  const [userProfile, setUserProfile] = useState(null);
+
+
+  useEffect(() => {
+    // authToken = token d'authentfi recup dans localStorage
+    const authToken = localStorage.getItem("authToken");
+
+    // Vérifiez si le token existe
+    if (authToken) {
+      fetchDataProfile(authToken);
+    }
+  }, []);
+
+  // fonction pour recupérer les donné des profile
+ async function fetchDataProfile(authToken) {
+
+  try {
+    const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: JSON.stringify({
+        status: 0,
+        message: "string",
+        body: {
+          id: "string",
+          email: "string",
+        },
+      }),
+    });
+        // si HTTP ok (statut 200)
+        if (response.ok) {
+          // réponse en JSON
+          const responseData = await response.json();
+          console.log(responseData);
+          setUserProfile(responseData.body);
+          console.log(responseData.body);
+        } else {
+          console.error("Erreur :", response.statusText);
+      
+        }
+      } catch (error) {
+        console.error("Erreur :", error);  
+      }
+    };
+
+
+  
+
+
   return (
     <main className="bg-dark"> 
     <div className="header">
         <h1>
           Welcome back
           <br />
-          {/* {user.firstName + " " + user.lastName + " !"} */}
+          {userProfile.firstName} {userProfile.lastName}!
         </h1>
  </div>
 
